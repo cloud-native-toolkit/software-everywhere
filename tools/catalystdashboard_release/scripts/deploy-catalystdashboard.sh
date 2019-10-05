0ad996b34281dbf3dd59e3bbcf791b6d69806336
Sean Sundberg <seansund@us.ibm.com>
2019-10-05 09:15:11 -0500

    Wraps redirect-to-https values in quotes

--- tools/catalystdashboard_release/scripts/deploy-catalystdashboard.sh
+++ tools/catalystdashboard_release/scripts/deploy-catalystdashboard.sh
@@ -34,7 +34,7 @@ VALUES="ingress.hosts.0=${INGRESS_HOST}"
 if [[ -n "${TLS_SECRET_NAME}" ]]; then
     VALUES="${VALUES},ingress.tls[0].secretName=${TLS_SECRET_NAME}"
     VALUES="${VALUES},ingress.tls[0].hosts[0]=${INGRESS_HOST}"
-    VALUES="${VALUES},ingress.annotations.ingress\.bluemix\.net/redirect-to-https=true"
+    VALUES="${VALUES},ingress.annotations.ingress\.bluemix\.net/redirect-to-https=\"true\""
 fi
 
 echo "*** Generating kube yaml from helm template into ${OUTPUT_YAML}"
--- tools/sonarqube_release/scripts/deploy-sonarqube.sh
+++ tools/sonarqube_release/scripts/deploy-sonarqube.sh
@@ -62,7 +62,7 @@ VALUES=ingress.hosts.0.name="${SONARQUBE_HOST}"
 if [[ -n "${TLS_SECRET_NAME}" ]]; then
     VALUES="${VALUES},ingress.tls[0].secretName=${TLS_SECRET_NAME}"
     VALUES="${VALUES},ingress.tls[0].hosts[0]=${SONARQUBE_HOST}"
-    VALUES="${VALUES},ingress.annotations.ingress\.bluemix\.net/redirect-to-https=true"
+    VALUES="${VALUES},ingress.annotations.ingress\.bluemix\.net/redirect-to-https=\"true\""
 fi
 
 echo "*** Generating sonarqube yaml from helm template with plugins ${PLUGIN_YAML}"
