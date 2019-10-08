48f845d07ccbd7a58738d5df4d2c3f9198e9b747
Sean Sundberg <seansund@us.ibm.com>
2019-10-08 12:29:26 -0500

    Changed delete namespace script to delete the namespace directly instead of trying to clean up resources

--- cluster/namespaces/scripts/deleteNamespace.sh
+++ cluster/namespaces/scripts/deleteNamespace.sh
@@ -13,6 +13,6 @@ fi
 
 if [[ -n $(kubectl get namespaces -o jsonpath='{ range .items[*] }{ .metadata.name }{ "\n" }{ end }' | grep "${NAMESPACE}") ]]; then
     echo "*** Deleting namespace and contained resources: ${NAMESPACE}"
-    kubectl delete daemonsets,replicasets,services,deployments,pods,rc,ing,statefulsets,crds,secrets --all -n "${NAMESPACE}"
+#    kubectl delete daemonsets,replicasets,services,deployments,pods,rc,ing,statefulsets,crds,secrets --all -n "${NAMESPACE}"
     kubectl delete namespace "${NAMESPACE}"
 fi
