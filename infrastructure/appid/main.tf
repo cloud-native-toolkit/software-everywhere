d10f7fb5b6062b97c72c973962e5737b8e7f5ba0
Sean Sundberg <seansund@us.ibm.com>
2019-10-16 12:09:32 -0500

    Adds logic to generate key for appid

--- infrastructure/appid/main.tf
+++ infrastructure/appid/main.tf
@@ -17,6 +17,18 @@ resource "ibm_resource_instance" "appid_instance" {
   }
 }
 
+resource "ibm_resource_key" "appid_key" {
+  name                 = "appid"
+  role                 = "Writer"
+  resource_instance_id = "${ibm_resource_instance.appid_instance.id}"
+
+  //User can increase timeouts
+  timeouts {
+    create = "15m"
+    delete = "15m"
+  }
+}
+
 locals {
   namespaces      = ["${var.dev_namespace}", "${var.test_namespace}", "${var.staging_namespace}"]
   namespace_count = 3
@@ -25,10 +37,11 @@ locals {
 resource "ibm_container_bind_service" "appid_service_binding" {
   count = "${local.namespace_count}"
 
-  cluster_name_id             = "${var.cluster_id}"
-  service_instance_name       = "${ibm_resource_instance.appid_instance.name}"
-  namespace_id                = "${local.namespaces[count.index]}"
-  resource_group_id           = "${data.ibm_resource_group.tools_resource_group.id}"
+  cluster_name_id       = "${var.cluster_id}"
+  service_instance_name = "${ibm_resource_instance.appid_instance.name}"
+  namespace_id          = "${local.namespaces[count.index]}"
+  resource_group_id     = "${data.ibm_resource_group.tools_resource_group.id}"
+  key                   = "${ibm_resource_key.appid_key.name}"
 
   // The provider (v16.1) is incorrectly registering that these values change each time,
   // this may be removed in the future if this is fixed.
