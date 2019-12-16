363bd37aeaa637ea28cf08f214faba967b5676a7
Sean Sundberg <seansund@us.ibm.com>
2019-12-16 15:47:32 -0600

    Namespaces: Cleans up logic to test for existence of namespace and delete it

--- generic/cluster/namespaces/scripts/deleteNamespace.sh
+++ generic/cluster/namespaces/scripts/deleteNamespace.sh
@@ -11,9 +11,5 @@ if [[ -n "${KUBECONFIG_IKS}" ]]; then
     export KUBECONFIG="${KUBECONFIG_IKS}"
 fi
 
-if [[ -n $(kubectl get namespaces -o jsonpath='{ range .items[*] }{ .metadata.name }{ "\n" }{ end }' | grep "${NAMESPACE}") ]]; then
-    echo "*** Deleting namespace and contained resources: ${NAMESPACE}"
-#    kubectl delete daemonsets,replicasets,services,deployments,pods,rc,ing,statefulsets,crds,secrets --all -n "${NAMESPACE}"
-    kubectl delete namespace "${NAMESPACE}" 1> /dev/null 2> /dev/null
-    exit 0
-fi
+kubectl get namespaces "${NAMESPACE}" 1> /dev/null 2> /dev/null && kubectl delete namespace "${NAMESPACE}"
+exit 0
