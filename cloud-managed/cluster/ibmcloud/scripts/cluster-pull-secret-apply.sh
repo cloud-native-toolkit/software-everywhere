701ac86ebaffabb9e0fc1b31a102258323802f92
Sean Sundberg <seansund@users.noreply.github.com>
2020-03-28 16:34:25 -0500

    IBMCloud Cluster module: Updates deprecated ibmcloud cli syntax (#50)

--- cloud-managed/cluster/ibmcloud/scripts/cluster-pull-secret-apply.sh
+++ cloud-managed/cluster/ibmcloud/scripts/cluster-pull-secret-apply.sh
@@ -9,7 +9,7 @@ fi
 if [[ $(kubectl get secrets -n default -o jsonpath='{ range .items[*] }{ .metadata.name }{ "\n" }{ end }' | grep icr | wc -l | xargs) -eq 0 ]]; then
     echo "Applying pull secrets to default namespace of cluster: ${CLUSTER_NAME}"
 
-    ibmcloud ks cluster-pull-secret-apply --cluster "${CLUSTER_NAME}"
+    ibmcloud ks cluster pull-secret apply --cluster "${CLUSTER_NAME}"
 else
     echo "Nothing to do... Secrets already applied to default namespace of cluster: ${CLUSTER_NAME}"
 fi
