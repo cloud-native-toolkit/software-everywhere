364c4e17af397952a88b9db6af3a6e5e4d281efb
Sean Sundberg <seansund@us.ibm.com>
2020-12-03 03:45:11 -0600

    Updates catalog build logic (#95)

--- .github/scripts/build-catalog.sh
+++ .github/scripts/build-catalog.sh
@@ -12,19 +12,31 @@ fi
 rm -rf "${DEST_DIR}"
 mkdir -p "${DEST_DIR}"
 
-cat "${BASE_DIR}/catalog.yaml" | grep -E "^[^ ]+:$" | sed "s/://g" | while read category; do
+yq r -j "${BASE_DIR}/catalog.yaml" | jq -r '.categories | .[] | .category' | while read category; do
   echo "*** category: ${category}"
-  yq r "${BASE_DIR}/catalog.yaml" "${category}" | sed -E "s/^ *- *//g" | while read module_url; do
+
+  yq r -j "${BASE_DIR}/catalog.yaml" | \
+    jq -r --arg CATEGORY "${category}" '.categories | .[] | select(.category == $CATEGORY) | del(.modules)' | \
+    yq r --prettyPrint - > "${DEST_DIR}/${category}.yaml"
+
+  yq r -j "${BASE_DIR}/catalog.yaml" | \
+    jq -r '.categories | .[] | select(.category == "cluster") | .modules | .[]' | \
+    while read module_url; do
+
     echo "module_url: ${module_url}"
-    curl -sL "${module_url}" | yq p - "[+]" >> "${DEST_DIR}/${category}.yaml"
+    curl -sL "${module_url}" | \
+      yq p - "[+]" | yq p - "modules" | yq m -i -a "${DEST_DIR}/${category}.yaml" -
   done
 done
 
 rm -f "${DEST_DIR}/index.yaml"
 
-ls "${DEST_DIR}"/*.yaml | while read category_file; do
-  category=$(basename "${category_file}" | sed -E "s/(.*).yaml/\1/g")
+echo "Touching ${DEST_DIR}/index.yaml"
+echo "" > "${DEST_DIR}/index.yaml"
 
-  yq p "${category_file}" "${category}" >> "${DEST_DIR}/index.yaml"
+echo "Merging categories"
+
+ls "${DEST_DIR}"/*.yaml | grep -v index.yaml | while read category_file; do
+  yq p "${category_file}" "[+]" | yq p - "categories" | yq m -i -a "${DEST_DIR}/index.yaml" -
   rm "${category_file}"
 done
