0cb7c87ae431737b0536b224220f9495cde29985
Sean Sundberg <seansund@us.ibm.com>
2022-01-06 23:28:19 -0600

    Updates UI to use hash router (#157)
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
+import {HashRouter, Route, Routes} from 'react-router-dom';
 import {Content, Header, HeaderMenu, HeaderMenuItem, HeaderName, HeaderNavigation} from 'carbon-components-react';
 
 import './app.scss';
-import {CatalogPage} from './pages';
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
-import {ContributingPage} from './pages/contributing/ContributingPage';
-import {HowToPage} from './pages/how-to/HowToPage';
-import {HowToTerraformPage} from './pages/how-to/terraform/HowToTerraformPage';
-import {HowToGitopsPage} from './pages/how-to/gitops/HowToGitopsPage';
+import {CatalogPage, ContributingPage} from './pages';
+import {HowToGitopsPage, HowToPage, HowToTerraformPage} from './pages/how-to';
 
 class App extends React.Component<any, any> {
 
@@ -15,20 +12,20 @@ class App extends React.Component<any, any> {
     return (
       <div className="App">
         <Header aria-label="Automation modules">
-          <HeaderName href="/" prefix="">
+          <HeaderName href="#" prefix="">
             Automation modules
           </HeaderName>
           <HeaderNavigation aria-label="Automation modules">
             <HeaderMenuItem href="/">Module catalog</HeaderMenuItem>
-            <HeaderMenuItem href="/contributing">Contributing</HeaderMenuItem>
+            <HeaderMenuItem href="#/contributing">Contributing</HeaderMenuItem>
             <HeaderMenu aria-label="How to" menuLinkName="How To">
-              <HeaderMenuItem href="/how-to/terraform">Create a terraform module</HeaderMenuItem>
-              <HeaderMenuItem href="/how-to/gitops">Create a gitops module</HeaderMenuItem>
+              <HeaderMenuItem href="#/how-to/terraform">Create a terraform module</HeaderMenuItem>
+              <HeaderMenuItem href="#/how-to/gitops">Create a gitops module</HeaderMenuItem>
             </HeaderMenu>
           </HeaderNavigation>
         </Header>
         <Content>
-          <BrowserRouter>
+          <HashRouter>
             <Routes>
               <Route path="/" element={<CatalogPage />} />
               <Route path="/contributing" element={<ContributingPage />} />
@@ -36,7 +33,7 @@ class App extends React.Component<any, any> {
               <Route path="/how-to/terraform" element={<HowToTerraformPage />} />
               <Route path="/how-to/gitops" element={<HowToGitopsPage />} />
             </Routes>
-          </BrowserRouter>
+          </HashRouter>
         </Content>
       </div>
     )
